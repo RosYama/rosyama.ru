894a5e72402e7a71bddadf76d220d7c92736924f
Fedor Ezeev <fezeev@gmail.com>
2012-01-17 16:48:05 +0300

    Поправил ссылку на страничку с подробной информацией

--- bitrix/templates/st1234/include_areas/highannounce.php
+++ bitrix/templates/st1234/include_areas/highannounce.php
@@ -4,7 +4,7 @@
     <tr><td style="padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px"> <b> 
       <p>Сбор средств на развитие РосЯмы:</p>
         <p>Яндекс-деньги: 41001550415485</p>
-        <p><a href="/donations.html" >Подробнее</a></p>
+        <p><a href="/donations/index.php" >Подробнее</a></p>
       </b>
     </td></tr>
   </tbody>
