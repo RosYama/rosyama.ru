39d7944c05646a67347b7e1b5b31a2bba140adaa
Fedor Ezeev <fezeev@gmail.com>
2012-01-17 16:45:24 +0300

    Добавил текст внутри квадратика в репо, а то он пропадал всё время

---
+++ bitrix/templates/st1234/include_areas/highannounce.php
@@ -0,0 +1,12 @@
+
+<table style="background-color: rgb(241, 109, 126)"> 
+  <tbody> 
+    <tr><td style="padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px"> <b> 
+      <p>Сбор средств на развитие РосЯмы:</p>
+        <p>Яндекс-деньги: 41001550415485</p>
+        <p><a href="/donations.html" >Подробнее</a></p>
+      </b>
+    </td></tr>
+  </tbody>
+</table>
+                                                                   
